e43bbe3e1f6cb0f141156fac56be73564f16507e
DevExpressExampleBot <fakeEmail>
2022-09-17 15:16:53 +0400

    Source auto update [skip ci]

--- VB/Sample/Controllers/HomeController.vb
+++ VB/Sample/Controllers/HomeController.vb
@@ -1,45 +1,42 @@
-Imports Microsoft.VisualBasic
 Imports System.Web.Mvc
 Imports DevExpress.Web.Mvc
 Imports Sample.Models
 
 Namespace Sample.Controllers
-	Public Class HomeController
-		Inherits Controller
-		Private list As New PersonsList()
 
-		Public Function Index() As ActionResult
-			Return View(list.GetPersons())
-		End Function
+    Public Class HomeController
+        Inherits Controller
 
-		Public Function GridViewEditingPartial() As ActionResult
-			Return PartialView(list.GetPersons())
-		End Function
+        Private list As PersonsList = New PersonsList()
 
-		Public Function CustomGridViewEditingPartial(ByVal key As Integer) As ActionResult
-			ViewData("key") = key
-			Return PartialView("GridViewEditingPartial", list.GetPersons())
-		End Function
+        Public Function Index() As ActionResult
+            Return View(list.GetPersons())
+        End Function
 
-		<HttpPost, ValidateInput(False)> _
-		Public Function EditingAddNew(<ModelBinder(GetType(DevExpressEditorsBinder))> ByVal person As Person) As ActionResult
-			If ModelState.IsValid Then
-				list.AddPerson(person)
-			End If
-			Return PartialView("GridViewEditingPartial", list.GetPersons())
-		End Function
+        Public Function GridViewEditingPartial() As ActionResult
+            Return PartialView(list.GetPersons())
+        End Function
 
-		<HttpPost, ValidateInput(False)> _
-		Public Function EditingUpdate(<ModelBinder(GetType(DevExpressEditorsBinder))> ByVal personInfo As Person) As ActionResult
-			If ModelState.IsValid Then
-				list.UpdatePerson(personInfo)
-			End If
-			Return PartialView("GridViewEditingPartial", list.GetPersons())
-		End Function
+        Public Function CustomGridViewEditingPartial(ByVal key As Integer) As ActionResult
+            ViewData("key") = key
+            Return PartialView("GridViewEditingPartial", list.GetPersons())
+        End Function
 
-		Public Function EditingDelete(ByVal personId As Integer) As ActionResult
-			list.DeletePerson(personId)
-			Return PartialView("GridViewEditingPartial", list.GetPersons())
-		End Function
-	End Class
+        <HttpPost, ValidateInput(False)>
+        Public Function EditingAddNew(<ModelBinder(GetType(DevExpressEditorsBinder))> ByVal person As Person) As ActionResult
+            If ModelState.IsValid Then list.AddPerson(person)
+            Return PartialView("GridViewEditingPartial", list.GetPersons())
+        End Function
+
+        <HttpPost, ValidateInput(False)>
+        Public Function EditingUpdate(<ModelBinder(GetType(DevExpressEditorsBinder))> ByVal personInfo As Person) As ActionResult
+            If ModelState.IsValid Then list.UpdatePerson(personInfo)
+            Return PartialView("GridViewEditingPartial", list.GetPersons())
+        End Function
+
+        Public Function EditingDelete(ByVal personId As Integer) As ActionResult
+            list.DeletePerson(personId)
+            Return PartialView("GridViewEditingPartial", list.GetPersons())
+        End Function
+    End Class
 End Namespace
